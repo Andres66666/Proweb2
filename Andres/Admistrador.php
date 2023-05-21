d261777fe392b44787d3151f834d3deb9cd79153
Andres66666 <cya2024773@est.univalle.edu>
2023-05-20 21:04:16 -0400

    modificacion andres

--- Andres/Admistrador.php
+++ Andres/Admistrador.php
@@ -1,40 +1,49 @@
 <?php
+session_start();
 
-ini_set("display_errors","on");
-$conexion = conectar_MySQL("sql308.byethost14.com","b14_33887233","13247291","b14_33887233_Salon");
+ini_set("display_errors", "on");
+$conexion = conectar_MySQL("sql308.byethost14.com", "b14_33887233", "13247291", "b14_33887233_Salon");
 
-function conectar_MySQL($host, $user, $pass, $bd){
-    $conexion = mysqli_connect($host, $user, $pass, $bd) or die ("Error al conectar : ".mysqli_connect_error());
-    mysqli_select_db($conexion, $bd) or die ("Error al seleccionar la BD: ".mysqli_error($conexion));
+function conectar_MySQL($host, $user, $pass, $bd)
+{
+    $conexion = mysqli_connect($host, $user, $pass, $bd) or die("Error al conectar: " . mysqli_connect_error());
+    mysqli_select_db($conexion, $bd) or die("Error al seleccionar la BD: " . mysqli_error($conexion));
     return $conexion;
 }
 
-function Administrador($conexion, $CI, $NOMBRES, $APELLIDO_PAT, $APELLIDO_MAT,$FECHA_NAC,$SEXO,$TELEFONO,$CORREO,$CATEGORIA,$IMG){
-    $sql = "INSERT INTO  Empleados VALUES (".$CI.",'".$NOMBRES."','".$APELLIDO_PAT."','".$APELLIDO_MAT."','".$FECHA_NAC."','".$SEXO."','".$TELEFONO."','".$CORREO."','".$CATEGORIA."','".$IMG."')";
-    return mysqli_query($conexion, $sql);
+// Función para verificar el inicio de sesión del administrador
+function autenticarAdministrador($conexion, $USUARIO, $CONTRASENIA)
+{
+    $sql = "SELECT * FROM Administrador WHERE USUARIO = '$USUARIO' AND CONTRASENIA = '$CONTRASENIA'";
+    $resultado = mysqli_query($conexion, $sql);
+
+    // Verificar si se obtuvo algún resultado
+    if (mysqli_num_rows($resultado) > 0) {
+        // Iniciar sesión y redirigir al panel de administración
+        $_SESSION['admin'] = true;
+        echo '<script>alert("Datos correctos");</script>';
+        header("Location:../index.html");
+        exit();
+    } else {
+        // Datos incorrectos, mostrar mensaje de alerta
+        echo '<script>alert("Usuario o contraseña incorrectos");</script>';
+    }
+
+    // Liberar memoria del resultado
+    mysqli_free_result($resultado);
 }
 
-$CI = $_POST['CI'];
-$NOMBRES = $_POST['NOMBRES'];
-$APELLIDO_PAT = $_POST['APELLIDO_PAT'];
-$APELLIDO_MAT = $_POST['APELLIDO_MAT'];
-$FECHA_NAC = $_POST['FECHA_NAC'];
-$SEXO = $_POST['SEXO'];
-$TELEFONO = $_POST['TELEFONO'];
-$CORREO = $_POST['CORREO'];
-$CATEGORIA = $_POST['CATEGORIA'];
-$IMG = $_POST['IMG'];
+// Verificar si se ha enviado el formulario de inicio de sesión
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $USUARIO = $_POST['USUARIO'];
+    $CONTRASENIA = $_POST['CONTRASENIA'];
 
-
-$ok = Administrador($conexion, $CI, $NOMBRES, $APELLIDO_PAT, $APELLIDO_MAT,$FECHA_NAC,$SEXO,$TELEFONO,$CORREO,$CATEGORIA,$IMG);
-
-if ($ok == false){
-    echo "Error al insertar datos<br/>";
-}else{
-    echo "Datos insertados correctamente<br/>";
+    // Autenticar al administrador
+    autenticarAdministrador($conexion, $USUARIO, $CONTRASENIA);
 }
 
-
-
+mysqli_close($conexion);
 ?>
-
+    <?php if (isset($error)) : ?>
+        <p><?php echo $error; ?></p>
+    <?php endif; ?>
---
+++ Andres/RegistroEmpleado.php
@@ -0,0 +1,57 @@
+<?php
+
+ini_set("display_errors","on");
+$conexion = conectar_MySQL("sql308.byethost14.com","b14_33887233","13247291","b14_33887233_Salon");
+
+function conectar_MySQL($host, $user, $pass, $bd){
+    $conexion = mysqli_connect($host, $user, $pass, $bd) or die ("Error al conectar: " . mysqli_connect_error());
+    mysqli_select_db($conexion, $bd) or die ("Error al seleccionar la BD: " . mysqli_error($conexion));
+    return $conexion;
+}
+
+/* Función para insertar empleados en la base de datos */
+function Empleado($conexion, $ID_EMPLEADO, $NOMBRE, $APELLIDO_PAT, $APELLIDO_MAT, $F_CONTRATACION, $SEXO, $ESPECIALIDAD, $TELEFONO, $E_MAIL, $FOTO, $ID_ADMINISTRADOR){
+    $sql = "INSERT INTO EMPLEADO VALUES ('$ID_EMPLEADO', '$NOMBRE', '$APELLIDO_PAT', '$APELLIDO_MAT', '$F_CONTRATACION', '$SEXO', '$ESPECIALIDAD', '$TELEFONO', '$E_MAIL', '$FOTO', '$ID_ADMINISTRADOR')";
+    return mysqli_query($conexion, $sql);
+}
+
+$ID_EMPLEADO = $_POST['ID_EMPLEADO'];
+$NOMBRE = $_POST['NOMBRE'];
+$APELLIDO_PAT = $_POST['APELLIDO_PAT'];
+$APELLIDO_MAT = $_POST['APELLIDO_MAT'];
+$F_CONTRATACION = $_POST['F_CONTRATACION'];
+$SEXO = $_POST['SEXO'];
+$ESPECIALIDAD = $_POST['ESPECIALIDAD'];
+$TELEFONO = $_POST['TELEFONO'];
+$E_MAIL = $_POST['E_MAIL'];
+$FOTO = $_POST['FOTO'];
+
+// Realizar consulta para obtener el ID del administrador
+$sql = "SELECT ID_ADMINISTRADOR FROM Administrador";
+$resultado = mysqli_query($conexion, $sql);
+
+// Verificar si se obtuvo algún resultado
+if (mysqli_num_rows($resultado) > 0) {
+    // Obtener el primer registro
+    $row = mysqli_fetch_assoc($resultado);
+    $ID_ADMINISTRADOR = $row['ID_ADMINISTRADOR'];
+
+    $ok = Empleado($conexion, $ID_EMPLEADO, $NOMBRE, $APELLIDO_PAT, $APELLIDO_MAT, $F_CONTRATACION, $SEXO, $ESPECIALIDAD, $TELEFONO, $E_MAIL, $FOTO, $ID_ADMINISTRADOR);
+
+    if ($ok == false){
+        echo "Error al insertar datos<br/>";
+    } else {
+        echo "Datos insertados correctamente<br/>";
+    }
+
+    // Asignar el valor del ID_ADMINISTRADOR al campo correspondiente en el formulario
+    echo '<script>document.getElementsByName("ID_ADMINISTRADOR")[0].value = "' . $ID_ADMINISTRADOR . '";</script>';
+} else {
+    echo "No se encontró ningún ID de administrador<br/>";
+}
+
+// Liberar memoria del resultado
+mysqli_free_result($resultado);
+
+mysqli_close($conexion);
+?>
