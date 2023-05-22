f030a3916dd99c302ce7e1d609fe177d7ba64bcb
Hugoojr <hugoemanuelmonzoncarrasco@gmail.com>
2023-05-22 00:03:30 -0400

    valio

--- Hugo/config.php
+++ Hugo/config.php
@@ -1,9 +1,7 @@
 <?php
-$usuario  = "root";
-$password = "";
-$servidor = "localhost";
-$basededatos = "practicas";
-$con = mysqli_connect($servidor, $usuario, $password) or die("No se ha podido conectar al Servidor");
-$db = mysqli_select_db($con, $basededatos) or die("Upps! Error en conectar a la Base de Datos");
-?>
+ini_set("display_errors","on");
 
+$con = mysqli_connect("sql308.byethost14.com","b14_33887233","13247291","b14_33887233_Salon") or die ("Error al conectar: " . mysqli_connect_error());
+mysqli_select_db($con, $bd) or die ("Error al seleccionar la BD: " . mysqli_error($con));
+
+?>
