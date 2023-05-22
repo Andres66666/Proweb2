9baee7267f27188bd66e45df8438f1bf5b254af9
Hugoojr <hugoemanuelmonzoncarrasco@gmail.com>
2023-05-22 01:09:59 -0400

    listo

--- Hugo/config.php
+++ Hugo/config.php
@@ -1,7 +1,8 @@
 <?php
-ini_set("display_errors","on");
-
-$con = mysqli_connect("sql308.byethost14.com","b14_33887233","13247291","b14_33887233_Salon") or die ("Error al conectar: " . mysqli_connect_error());
-mysqli_select_db($con, $bd) or die ("Error al seleccionar la BD: " . mysqli_error($con));
-
+$usuario  = "b14_33887233";
+$password = "13247291";
+$servidor = "sql308.byethost14.com";
+$basededatos = "b14_33887233_Salon";
+$con = mysqli_connect($servidor, $usuario, $password, $basededatos) or die("No se ha podido conectar al Servidor");
+$db = mysqli_select_db($con, $basededatos) or die("Upps! Error en conectar a la Base de Datos");
 ?>
