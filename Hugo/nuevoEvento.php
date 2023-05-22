061256f496865e100810c563eba02ece37fb6cc7
Hugoojr <hugoemanuelmonzoncarrasco@gmail.com>
2023-05-22 02:02:58 -0400

    ahora?

--- Hugo/nuevoEvento.php
+++ Hugo/nuevoEvento.php
@@ -3,15 +3,7 @@ date_default_timezone_set("America/Bogota");
 setlocale(LC_ALL,"es_ES");
 //$hora = date("g:i:A");
 
-ini_set("display_errors","on");
-$conexion = conectar_MySQL("sql308.byethost14.com","b14_33887233","13247291","b14_33887233_Salon");
-
-function conectar_MySQL($host, $user, $pass, $bd){
-    $conexion = mysqli_connect($host, $user, $pass, $bd) or die ("Error al conectar: " . mysqli_connect_error());
-    mysqli_select_db($conexion, $bd) or die ("Error al seleccionar la BD: " . mysqli_error($conexion));
-    return $conexion;
-}
-
+require("config.php");
 $evento            = ucwords($_REQUEST['evento']);
 $f_inicio          = $_REQUEST['fecha_inicio'];
 $fecha_inicio      = date('Y-m-d', strtotime($f_inicio)); 
