a1f5e7774400a217f1d0fccbe9ebd2f961e5a5e0
Hugoojr <hugoemanuelmonzoncarrasco@gmail.com>
2023-06-01 09:10:47 -0400

    x

--- prueba/UpdateEvento.php
+++ prueba/UpdateEvento.php
@@ -12,7 +12,6 @@ $fecha_inicio      = date('Y-m-d', strtotime($f_inicio));
 
 $f_fin             = $_REQUEST['fecha_fin']; 
 $seteando_f_final  = date('Y-m-d', strtotime($f_fin));  
-$fecha_fin1        = strtotime($seteando_f_final."+ 1 days");
 $fecha_fin         = date('Y-m-d', ($fecha_fin1));  
 $color_evento      = $_REQUEST['color_evento'];
 
